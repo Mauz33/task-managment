31add47267fd69f473cf8f3b20fb9281acee6b4e
Maxim Eshenko <eshenko.maxim@gmail.com>
2025-01-30 16:09:10 +0300

    app context + initial migration

--- Users.Service/Program.cs
+++ Users.Service/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
+builder.Services.AddDbContext<ApplicationContext>(opt =>
+    opt.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
